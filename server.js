7b35e59623d86a4cae39e286db0ff678ecfa7a30
wilker818 <wilkerferreira818@gmail.com>
2023-03-17 15:06:45 -0300

    chore: adjusted server

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+let express = require("express");
 
-const app = express();
+let app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/front-crud-tutorials'));
+app.use(express.static(__dirname + "/dist/front-crud-tutorials"));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/front-crud-tutorials/'}),
+app.get("/*", (req, res) =>
+  res.sendFile(__dirname + "dist/front-crud-tutorials/index.html")
 );
 
 // Start the app by listening on the default Heroku port
