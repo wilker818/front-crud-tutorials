9aa0b0b14a6b0a5dcc4cea6d9215612287b64574
wilker818 <wilkerferreira818@gmail.com>
2023-03-17 16:55:05 -0300

    feat(app/router): added scroll behavior to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.onActivate;
+  }
   title = 'front-crud-tutorials';
+  onActivate(event: any) {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
 }
